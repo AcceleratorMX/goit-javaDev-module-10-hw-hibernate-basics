4c7ed9431008fe53b1febf79a70995982282c45c
AcceleratorMX <redfield242@gmail.com>
2023-06-13 15:59:30 +0300

    m10

--- src/main/java/ua/goit/hw10/DatabaseInitService.java
+++ src/main/java/ua/goit/hw10/DatabaseInitService.java
@@ -5,18 +5,18 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class DatabaseInitService {
-    private static String hibernatePath = "";
+    private static String url = "";
 
     public String getProperties() {
         Properties properties = new Properties();
         try {
             ClassLoader classLoader = getClass().getClassLoader();
             properties.load(classLoader.getResourceAsStream("hibernate.properties"));
-            hibernatePath = properties.getProperty("hibernate.connection.url");
+            url = properties.getProperty("hibernate.connection.url");
         } catch (IOException e) {
             throw new RuntimeException("Failed to load database.", e);
         }
-        return hibernatePath;
+        return url;
     }
 
     public void initDB() {
